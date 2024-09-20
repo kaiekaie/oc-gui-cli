091b9d27db4d123f7e03f3af8143d6bed274e441
magnusaga <magnus.aga@statnett.no>
2024-09-20 11:29:57 +0200

    Develop (#4)
    * changed release title
    
    * added tesbuild
    
    * added new version
    
    * added new version
    
    * fixed button on logs
    
    * fixed button styling
    
    * fixed button styling for table
    
    * updated pversion

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import type { Snippet } from 'svelte';
+let { data, header, row }: {
+		data: any[];
+		header: Snippet;
+		row: Snippet<[any]>;
+	} = $props();
+</script>
+
+<table class="table-auto w-full">
+	{#if header}
+		<thead>
+			<tr>{@render header()}</tr>
+		</thead>
+	{/if}
+
+	<tbody>
+		{#each data as d}
+			<tr>{@render row(d)}</tr>
+		{/each}
+	</tbody>
+</table>
+
